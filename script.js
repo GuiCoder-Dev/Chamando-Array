7caa6a576f40705fc776e1f0912f313b6f438588
GuiCoderzin <guimalagutti07@gmail.com>
2025-04-29 23:05:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let array = document.getElementById("array");
+
+let pc = ["CPU", "GPU", "MemóriaRam", "Placamãe", "SSD", "Watercooler", "Fans"]
+
+function chamada(){
+let number = prompt("Digite um número de 0 a 6")
+if(number >= 0 && number <=6) {
+    array.innerHTML = pc[number];
+} else if(number < 0 || number > 6){
+    array.innerHTML = "Digite um número de 1 a 6"
+} if (number == "") {
+    array.innerHTML = "Digite um número de 1 a 6"
+}
+
+}
+
